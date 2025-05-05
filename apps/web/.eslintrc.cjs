1185917cfe8cd6dae5a1fcc0e08eb4134b34294c
Dalin  Oluoch <mcdalinoluoch@gmail.com>
2025-05-05 12:26:01 +0300

    update

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ['@gonasi/eslint-config/web.js', 'plugin:storybook/recommended'],
+};
--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,7 +1,13 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  presets: [vercelPreset()],
+  // return a list of URLs to prerender at build time
+  async prerender() {
+    return ['/'];
+  },
 } satisfies Config;
