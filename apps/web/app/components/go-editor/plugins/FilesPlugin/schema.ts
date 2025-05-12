e15fdfdd9421b025e6d406763619637190d73747
gonasi <gonasiapp@gmail.com>
2025-05-12 15:57:25 +0300

    draft

--- apps/web/app/components/go-editor/plugins/FilesPlugin/schema.ts
+++ apps/web/app/components/go-editor/plugins/FilesPlugin/schema.ts
@@ -1,18 +1,5 @@
 import { z } from 'zod';
 
-import { NewFileSchema } from '@gonasi/schemas/file';
-
-const AltTextSchema = z
-  .string({ required_error: 'Please enter alternative text for the image.' })
-  .min(3, { message: 'Alternative text must be at least 3 characters long.' })
-  .max(100, { message: 'Alternative text cannot exceed 100 characters.' })
-  .refine((val) => /^[A-Za-z]+(?: [A-Za-z]+)*$/.test(val), {
-    message:
-      'Use only letters with single spaces between words. No extra spaces at the start or end.',
-  });
-
-// NewEditorFileSchema definition
 export const NewEditorFileSchema = z.object({
-  altText: AltTextSchema,
-  src: NewFileSchema,
+  fileId: z.string(),
 });
