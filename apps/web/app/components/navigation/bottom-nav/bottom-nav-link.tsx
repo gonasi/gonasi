e013d1d40262e96b899e81787bb017c587cf93c9
gonasi <gonasiapp@gmail.com>
2025-05-31 20:25:45 +0300

    update pulse animation position

--- apps/web/app/components/navigation/bottom-nav/bottom-nav-link.tsx
+++ apps/web/app/components/navigation/bottom-nav/bottom-nav-link.tsx
@@ -6,10 +6,9 @@ import { cn } from '~/lib/utils';
 interface Props {
   icon: LucideIcon;
   to: string;
-  name: string;
 }
 
-export function BottomNavLink({ icon: Icon, to, name }: Props) {
+export function BottomNavLink({ icon: Icon, to }: Props) {
   return (
     <NavLink to={to}>
       {({ isActive, isPending }) => {
@@ -20,18 +19,16 @@ export function BottomNavLink({ icon: Icon, to, name }: Props) {
         );
 
         const dotClass = cn(
-          'h-1 w-1 rounded-full',
+          'absolute h-1 w-1 rounded-full',
+          'top-0 -right-1',
           isPending ? 'bg-primary animate-ping' : 'bg-transparent',
         );
 
         return (
           <div className={containerClass} aria-disabled={isActive}>
-            <div className='flex flex-col items-center gap-1'>
-              <Icon size={24} strokeWidth={isActive ? 2 : 1} />
-              <div className='flex gap-1'>
-                <span className='ml-1 text-xs'>{name}</span>
-                <span className={dotClass} />
-              </div>
+            <div className='relative'>
+              <Icon size={28} strokeWidth={isActive ? 2 : 1} />
+              <span className={dotClass} />
             </div>
           </div>
         );
--- apps/web/app/components/navigation/bottom-nav/bottom-nav.tsx
+++ apps/web/app/components/navigation/bottom-nav/bottom-nav.tsx
@@ -15,8 +15,8 @@ export function BottomNav({ user }: Props) {
     <nav className='fixed bottom-0 h-18 w-full px-0 md:hidden'>
       <div className='from-secondary/30 to-primary/30 h-0.5 bg-gradient-to-r' />
       <div className='flex h-full items-center justify-between px-4'>
-        <BottomNavLink icon={House} to='/' name='Home' />
-        <BottomNavLink icon={Telescope} to='/explore' name='Explore' />
+        <BottomNavLink icon={House} to='/' />
+        <BottomNavLink icon={Telescope} to='/explore' />
         <div>
           {user ? (
             <div>
@@ -28,13 +28,14 @@ export function BottomNav({ user }: Props) {
                       imageUrl={user.avatar_url}
                       isActive={isActive}
                       isPending={isPending}
+                      size='sm'
                     />
                   </span>
                 )}
               </NavLink>
             </div>
           ) : (
-            <BottomNavLink icon={UserRound} to='/login' name='Account' />
+            <BottomNavLink icon={UserRound} to='/login' />
           )}
         </div>
       </div>
