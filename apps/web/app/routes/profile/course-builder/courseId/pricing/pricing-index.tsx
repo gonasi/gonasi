bfdd660df43557d904c5b4b5d3f0f9740cc9f270
gonasi <gonasiapp@gmail.com>
2025-06-10 21:47:26 +0300

    pricing draft page

--- apps/web/app/routes/profile/course-builder/courseId/pricing/pricing-index.tsx
+++ apps/web/app/routes/profile/course-builder/courseId/pricing/pricing-index.tsx
@@ -1,3 +1,16 @@
+import { BannerCard } from '~/components/cards';
+
 export default function CoursePricing() {
-  return <h1>Course pricing goes here</h1>;
+  return (
+    <div className='mx-auto max-w-2xl'>
+      <BannerCard
+        message='Your new prices go live instantly, no need to hit publish!'
+        description='Just updating the price? It updates right away. Only content changes need publishing.'
+        variant='info'
+        className='mb-10'
+      />
+
+      <h2>pricing</h2>
+    </div>
+  );
 }
