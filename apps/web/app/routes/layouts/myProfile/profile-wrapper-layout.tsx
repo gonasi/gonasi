39da8dadf3b1ea640cb6a2da563237d10165815f
gonasi <gonasiapp@gmail.com>
2025-07-15 16:18:00 +0300

    loading top nav

--- apps/web/app/routes/layouts/myProfile/profile-wrapper-layout.tsx
+++ apps/web/app/routes/layouts/myProfile/profile-wrapper-layout.tsx
@@ -4,6 +4,7 @@ import { fetchActiveOrganizationAndMember } from '@gonasi/database/organizations
 
 import type { Route } from './+types/profile-wrapper-layout';
 
+import { TopNav } from '~/components/navigation/top-nav';
 import { ProfileTopNav } from '~/components/navigation/top-nav/profile-top-nav';
 import { createClient } from '~/lib/supabase/supabase.server';
 import { useStore } from '~/store';
@@ -20,13 +21,20 @@ export default function ProfileWrapperLayout({ loaderData }: Route.ComponentProp
 
   return (
     <div>
-      <ProfileTopNav
-        user={activeUserProfile}
-        organization={loaderData?.organization}
-        member={loaderData?.member ?? undefined}
-        showBackArrow
-        loading={isActiveUserProfileLoading}
-      />
+      {isActiveUserProfileLoading ? (
+        <nav className='border-b-card hidden h-16 w-full animate-pulse items-center border-b md:flex md:h-20 md:px-4' />
+      ) : activeUserProfile ? (
+        <ProfileTopNav
+          user={activeUserProfile}
+          organization={loaderData?.organization}
+          member={loaderData?.member ?? undefined}
+          showBackArrow
+          loading={isActiveUserProfileLoading}
+        />
+      ) : (
+        <TopNav user={activeUserProfile} />
+      )}
+
       <section className='container mx-auto min-h-screen'>
         <Outlet />
       </section>
