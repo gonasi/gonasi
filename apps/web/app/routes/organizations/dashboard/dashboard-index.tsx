5d24af95a29f5aebdd8e2c3322daf87a8c6307d6
gonasi <gonasiapp@gmail.com>
2025-10-25 15:38:12 +0300

    draft organization plan card view

--- apps/web/app/routes/organizations/dashboard/dashboard-index.tsx
+++ apps/web/app/routes/organizations/dashboard/dashboard-index.tsx
@@ -19,7 +19,7 @@ export default function DashboardIndex() {
       </div>
 
       {/* Plan, Storage, and Completion */}
-      <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3'>
         <OrganizationPlanCard />
         <StorageCard />
         <StatsCard
